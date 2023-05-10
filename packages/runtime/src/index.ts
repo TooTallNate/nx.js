035cb0c82d13c981f1275a030fdba95ed7168d03
Nathan Rajlich <n@n8.io>
2023-05-10 01:01:01 -0700

    Add `setTimeout()` and related functions

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -1,5 +1,6 @@
 import { def } from './polyfills';
 import { Switch as _Switch } from './switch';
+import { createTimersFactory } from './timers';
 import { Console } from './console';
 
 export type { Switch } from './switch';
@@ -8,3 +9,10 @@ const Switch = new _Switch();
 def('Switch', Switch);
 
 def('console', new Console(Switch));
+
+const { setTimeout, setInterval, clearTimeout, clearInterval } =
+	createTimersFactory(Switch);
+def('setTimeout', setTimeout);
+def('setInterval', setInterval);
+def('clearTimeout', clearTimeout);
+def('clearInterval', clearInterval);
---
+++ packages/runtime/src/timers.ts
@@ -0,0 +1,62 @@
+import type { Switch } from './switch';
+
+interface Timer {
+	args: any[];
+	callback: Function;
+	invokeAt: number;
+	interval?: number;
+}
+
+export function createTimersFactory(s: Switch) {
+	let nextId = 0;
+	const timers = new Map<number, Timer>();
+
+	function setTimeout(handler: TimerHandler, timeout = 0, ...args: any[]) {
+		const id = ++nextId;
+		const callback =
+			typeof handler === 'string' ? new Function(handler) : handler;
+		timers.set(id, {
+			args,
+			callback,
+			invokeAt: Date.now() + timeout,
+		});
+		return id;
+	}
+
+	function setInterval(handler: TimerHandler, timeout = 0, ...args: any[]) {
+		const id = ++nextId;
+		const callback =
+			typeof handler === 'string' ? new Function(handler) : handler;
+		timers.set(id, {
+			args,
+			callback,
+			invokeAt: Date.now() + timeout,
+			interval: timeout,
+		});
+		return id;
+	}
+
+	function clearTimeout(id: number) {
+		timers.delete(id);
+	}
+
+	function clearInterval(id: number) {
+		timers.delete(id);
+	}
+
+	s.addEventListener('frame', () => {
+		const now = Date.now();
+		for (const [id, timer] of timers) {
+			if (now >= timer.invokeAt) {
+				timer.callback.apply(null, timer.args);
+				if (typeof timer.interval === 'number') {
+					timer.invokeAt = now + timer.interval;
+				} else {
+					clearTimeout(id);
+				}
+			}
+		}
+	});
+
+	return { setTimeout, setInterval, clearTimeout, clearInterval };
+}
