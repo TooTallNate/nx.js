cb772c6714f4b10b8bfa43b0f81dc98277e3696d
Nathan Rajlich <n@n8.io>
2023-05-17 18:08:07 -0700

    Add "ttf-font" example

---
+++ apps/ttf-font/src/main.ts
@@ -0,0 +1,21 @@
+const ctx = Switch.screen.getContext('2d');
+
+// Place the font file alongside the `.nro` file on your SD card,
+// or embed it into the RomFS and read from there.
+const fontData = Switch.readFileSync(new URL('Alexandria.ttf', Switch.cwd()));
+const font = new FontFace('Alexandria', fontData);
+Switch.fonts.add(font);
+
+ctx.fillRect(0, 0, Switch.screen.width, Switch.screen.height);
+
+ctx.font = '36px Alexandria';
+ctx.fillStyle = 'white';
+ctx.fillText('This is the "Alexandria" font', 330, 330);
+
+ctx.font = '28px Alexandria';
+ctx.fillStyle = 'rgb(230, 200, 200)';
+ctx.fillText('Created by "Teaito"', 460, 390);
+
+ctx.font = '24px system-ui';
+ctx.fillStyle = 'rgb(200, 200, 230)';
+ctx.fillText('Press + to exit', Switch.screen.width - 190, 30);
