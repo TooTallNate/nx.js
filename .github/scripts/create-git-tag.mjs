f05274b981bf6b2b07e4ebbf71a3e586273175ad
Nathan Rajlich <n@n8.io>
2023-05-28 04:10:52 -0700

    .

--- .github/scripts/create-git-tag.mjs
+++ .github/scripts/create-git-tag.mjs
@@ -8,7 +8,7 @@ try {
 	// specific format in order to set the "published" output, which we
 	// rely on in order to trigger the job to create the GitHub Release.
 	// https://github.com/changesets/action/blob/595655c3eae7136ff5ba18200406898904362926/src/run.ts#LL96C1-L96C1
-	console.log(`New tag: nxjs@${tag}`);
+	console.log(`New tag: nxjs-runtime@${tag}`);
 } catch (err) {
 	if (err.message.includes('already exists')) {
 		console.log(`Skipping since git tag "${tag}" already exists`);
