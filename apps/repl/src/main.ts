fc7f1a5da7cc2aa71bcf5c8876531b205e06d3ec
Nathan Rajlich <n@n8.io>
2023-06-01 02:19:46 -0700

    repl: add support for mid-buffer insersion/deletion and history up/down arrow keys

--- apps/repl/src/main.ts
+++ apps/repl/src/main.ts
@@ -1,4 +1,4 @@
-import { cursor, erase } from 'sisteransi';
+import { erase } from 'sisteransi';
 import { bold, cyan, bgYellow } from 'kleur/colors';
 import { inspect } from './inspect';
 
@@ -11,32 +11,38 @@ let buffer = '';
 let historyIndex = 0;
 let cursorPosition = 0;
 const history: string[] = [];
-const prompt = bold(cyan('>'));
+const prompt = bold(cyan('> '));
 
 const cursorChar = (v: string) => bold(bgYellow(v || ' '));
 
 function renderPrompt() {
-	const bufferL = buffer.slice(0, cursorPosition);
-	const bufferP = buffer[cursorPosition];
-	const bufferR = buffer.slice(cursorPosition + 1);
-	Switch.print(
-		`\r${erase.line}${prompt} ${bufferL}${cursorChar(bufferP)}${bufferR}`
-	);
+	let b = buffer;
+	if (cursorPosition >= 0) {
+		const bufferL = buffer.slice(0, cursorPosition);
+		const bufferP = buffer[cursorPosition];
+		const bufferR = buffer.slice(cursorPosition + 1);
+		b = `${bufferL}${cursorChar(bufferP)}${bufferR}`;
+	}
+	Switch.print(`\r${erase.line}${prompt}${b}`);
 }
 
 Switch.addEventListener('keydown', (e) => {
 	const { key } = e;
 	if (key.length === 1) {
 		// Printable character
-		buffer += key;
+		buffer = `${buffer.slice(0, cursorPosition)}${key}${buffer.slice(
+			cursorPosition
+		)}`;
 		cursorPosition++;
 		renderPrompt();
 	} else if (key === 'Enter') {
 		// Remove cursor
-		Switch.print(`${cursor.backward(1)}${erase.lineEnd}\n`);
+		cursorPosition = -1;
+		renderPrompt();
+		Switch.print('\n');
 		try {
 			history.push(buffer);
-			historyIndex = history.length - 1;
+			historyIndex = history.length;
 			const trimmed = buffer.trim();
 			const result =
 				trimmed.length === 0 ? undefined : eval(`(${trimmed})`);
@@ -55,14 +61,24 @@ Switch.addEventListener('keydown', (e) => {
 		renderPrompt();
 	} else if (key === 'Backspace') {
 		if (buffer.length) {
-			buffer = buffer.slice(0, -1);
+			buffer = `${buffer.slice(0, cursorPosition - 1)}${buffer.slice(
+				cursorPosition
+			)}`;
 			cursorPosition--;
 			renderPrompt();
 		}
-	} else if (key === 'Escape') {
-		Switch.exit();
 	} else if (key === 'ArrowUp') {
+		if (historyIndex > 0) {
+			buffer = history[--historyIndex];
+			cursorPosition = buffer.length;
+			renderPrompt();
+		}
 	} else if (key === 'ArrowDown') {
+		if (historyIndex < history.length) {
+			buffer = history[++historyIndex] ?? '';
+			cursorPosition = buffer.length;
+			renderPrompt();
+		}
 	} else if (key === 'ArrowLeft') {
 		if (cursorPosition > 0) {
 			cursorPosition--;
@@ -73,6 +89,8 @@ Switch.addEventListener('keydown', (e) => {
 			cursorPosition++;
 			renderPrompt();
 		}
+	} else if (key === 'Escape') {
+		Switch.exit();
 	}
 });
 
