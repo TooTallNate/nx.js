02b388c229ed39245327835377718ef1425fb3c4
Nathan Rajlich <n@n8.io>
2023-06-01 02:21:36 -0700

    Make `console.log` be a bound function

---
+++ .changeset/strange-lemons-talk.md
@@ -0,0 +1,5 @@
+---
+'nxjs-runtime': patch
+---
+
+Make `console.log` be a bound function
--- packages/runtime/src/console.ts
+++ packages/runtime/src/console.ts
@@ -6,12 +6,13 @@ export class Console {
 
 	constructor(s: Switch) {
 		this[INTERNAL_SYMBOL] = s;
+		this.log = this.log.bind(this);
 	}
 
-	log = function log(this: Console, input: unknown) {
+	log(input: unknown) {
 		if (arguments.length === 0) input = '';
 		this[INTERNAL_SYMBOL].print(
 			`${typeof input === 'string' ? input : JSON.stringify(input)}\n`
 		);
-	};
+	}
 }
