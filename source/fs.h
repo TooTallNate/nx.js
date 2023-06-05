f9cf29ede274a0674cba00606540da35b61a9109
Nathan Rajlich <n@n8.io>
2023-06-05 01:30:53 -0700

    Break out filesystem functions into separate file

---
+++ source/fs.h
@@ -0,0 +1,10 @@
+#ifndef _NX_FS_
+#define _NX_FS_
+
+#include <switch.h>
+#include <quickjs/quickjs.h>
+
+JSValue js_read_file_sync(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv);
+JSValue js_readdir_sync(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv);
+
+#endif
--- source/main.c
+++ source/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <dirent.h>
 #include <errno.h>
 #include <string.h>
 #include <inttypes.h>
@@ -12,6 +11,7 @@
 #include <ft2build.h>
 
 #include "types.h"
+#include "fs.h"
 
 // Text renderer
 static PrintConsole *print_console = NULL;
@@ -221,81 +221,6 @@ static JSValue js_hid_get_keyboard_states(JSContext *ctx, JSValueConst this_val,
         JS_SetPropertyUint32(ctx, obj, i, JS_NewBigUint64(ctx, state.keys[i]));
     }
     return obj;
-}
-
-static JSValue js_readdir_sync(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv)
-{
-    DIR *dir;
-    struct dirent *entry;
-
-    const char *path = JS_ToCString(ctx, argv[0]);
-    dir = opendir(path);
-    if (dir == NULL)
-    {
-        JSValue error = JS_NewString(ctx, "An error occurred");
-        JS_Throw(ctx, error);
-        return JS_UNDEFINED;
-    }
-
-    int i = 0;
-    JSValue arr = JS_NewArray(ctx);
-    while ((entry = readdir(dir)) != NULL)
-    {
-        // Filter out `.` and `..`
-        if (!strcmp(".", entry->d_name) || !strcmp("..", entry->d_name))
-        {
-            continue;
-        }
-        JS_SetPropertyUint32(ctx, arr, i, JS_NewString(ctx, entry->d_name));
-        i++;
-    }
-
-    closedir(dir);
-
-    return arr;
-}
-
-static void free_js_array_buffer(JSRuntime *rt, void *opaque, void *ptr)
-{
-    js_free_rt(rt, ptr);
-}
-
-static JSValue js_read_file_sync(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv)
-{
-    const char *filename = JS_ToCString(ctx, argv[0]);
-    FILE *file = fopen(filename, "rb");
-    if (file == NULL)
-    {
-        JS_ThrowTypeError(ctx, "File not found: %s", filename);
-        JS_FreeCString(ctx, filename);
-        return JS_EXCEPTION;
-    }
-
-    fseek(file, 0, SEEK_END);
-    size_t size = ftell(file);
-    rewind(file);
-
-    uint8_t *buffer = js_malloc(ctx, size);
-    if (buffer == NULL)
-    {
-        JS_FreeCString(ctx, filename);
-        fclose(file);
-        JS_ThrowOutOfMemory(ctx);
-        return JS_EXCEPTION;
-    }
-
-    size_t result = fread(buffer, 1, size, file);
-    fclose(file);
-
-    if (result != size)
-    {
-        JS_FreeCString(ctx, filename);
-        js_free(ctx, buffer);
-        JS_ThrowTypeError(ctx, "Failed to read entire file. Got %lu, expected %lu", result, result);
-        return JS_EXCEPTION;
-    }
-
-    return JS_NewArrayBuffer(ctx, buffer, size, free_js_array_buffer, NULL, false);
 }
 
 static JSValue js_getenv(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv)
