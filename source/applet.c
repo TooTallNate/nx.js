d811ec980570c538c091d7fda6344a3b14277ab7
Nathan Rajlich <n@n8.io>
2023-06-13 00:56:47 -0700

    More js_ -> nx_

--- source/applet.c
+++ source/applet.c
@@ -1,19 +1,19 @@
 #include <switch.h>
 #include "applet.h"
 
-JSValue js_appletGetAppletType(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv)
+JSValue nx_appletGetAppletType(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv)
 {
     return JS_NewInt32(ctx, appletGetAppletType());
 }
 
-JSValue js_appletGetOperationMode(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv)
+JSValue nx_appletGetOperationMode(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv)
 {
     return JS_NewInt32(ctx, appletGetOperationMode());
 }
 
 static const JSCFunctionListEntry function_list[] = {
-    JS_CFUNC_DEF("appletGetAppletType", 0, js_appletGetAppletType),
-    JS_CFUNC_DEF("appletGetOperationMode", 0, js_appletGetOperationMode)};
+    JS_CFUNC_DEF("appletGetAppletType", 0, nx_appletGetAppletType),
+    JS_CFUNC_DEF("appletGetOperationMode", 0, nx_appletGetOperationMode)};
 
 void nx_init_applet(JSContext * ctx, JSValueConst native_obj)
 {
