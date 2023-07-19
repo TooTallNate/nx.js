cb343f0999daa9becaefa19c97d66d2e909e781a
Nathan Rajlich <n@n8.io>
2023-07-19 16:06:23 -0700

    Move `ImageData` to its own file

--- packages/runtime/src/canvas.ts
+++ packages/runtime/src/canvas.ts
@@ -2,6 +2,7 @@ import toPx = require('to-px/index.js');
 import colorRgba = require('color-rgba');
 import parseCssFont from 'parse-css-font';
 import { INTERNAL_SYMBOL } from './types';
+import { ImageData } from './canvas/image-data';
 import type { CanvasRenderingContext2DState } from './switch';
 import type { Switch as _Switch } from './switch';
 
@@ -25,47 +26,6 @@ export class Canvas {
 			);
 		}
 		return new CanvasRenderingContext2D(this);
-	}
-}
-
-export class ImageData implements globalThis.ImageData {
-	readonly colorSpace: PredefinedColorSpace;
-	readonly data: Uint8ClampedArray;
-	readonly height: number;
-	readonly width: number;
-
-	constructor(sw: number, sh: number, settings?: ImageDataSettings);
-	constructor(
-		data: Uint8ClampedArray,
-		sw: number,
-		sh?: number,
-		settings?: ImageDataSettings
-	);
-	constructor(
-		swOrData: number | Uint8ClampedArray,
-		shOrSw: number,
-		settingsOrSh?: ImageDataSettings | number,
-		settings?: ImageDataSettings
-	) {
-		let imageDataSettings: ImageDataSettings | undefined;
-		if (typeof swOrData === 'number') {
-			this.width = swOrData;
-			this.height = shOrSw;
-			if (typeof settingsOrSh !== 'number') {
-				imageDataSettings = settingsOrSh;
-			}
-			this.data = new Uint8ClampedArray(this.width * this.height * 4);
-		} else {
-			this.data = swOrData;
-			this.width = shOrSw;
-			if (typeof settingsOrSh === 'number') {
-				this.height = settingsOrSh;
-			} else {
-				this.height = (this.data.length / this.width) | 0;
-			}
-			imageDataSettings = settings;
-		}
-		this.colorSpace = imageDataSettings?.colorSpace || 'srgb';
 	}
 }
 
