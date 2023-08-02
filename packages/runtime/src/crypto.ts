d07c0eadff9919c4b69c01f7d76fb1455d7ec3bb
Nathan Rajlich <n@n8.io>
2023-08-01 18:59:56 -0700

    Optimize `crypto.randomUUID()`

--- packages/runtime/src/crypto.ts
+++ packages/runtime/src/crypto.ts
@@ -1,10 +1,12 @@
+import { def } from './utils';
 import { type Switch as _Switch } from './switch';
-import { bufferSourceToArrayBuffer, def } from './utils';
 
 declare const Switch: _Switch;
 
 class Crypto implements globalThis.Crypto {
-	subtle!: SubtleCrypto;
+	get subtle(): SubtleCrypto {
+        throw new Error('`crypto.subtle` is not yet implemented');
+    }
 
 	getRandomValues<T extends ArrayBufferView | null>(array: T): T {
 		if (array) {
@@ -18,12 +20,12 @@ class Crypto implements globalThis.Crypto {
 	}
 
 	randomUUID(): `${string}-${string}-${string}-${string}-${string}` {
+        let i = 0;
+        const bytes = new Uint8Array(31);
+        this.getRandomValues(bytes);
 		return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (v) => {
 			const c = +v;
-			return (
-				c ^
-				(this.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-			).toString(16);
+			return (c ^ (bytes[i++] & (15 >> (c / 4)))).toString(16);
 		}) as `${string}-${string}-${string}-${string}-${string}`;
 	}
 }
