86fdc499a4acd83f3d502de9c67e1b3ed1fd1da8
Nathan Rajlich <n@n8.io>
2023-09-29 16:21:53 -0300

    Clean up `apps/wasm` example

--- apps/wasm/src/main.ts
+++ apps/wasm/src/main.ts
@@ -2,8 +2,8 @@
 // which invokes the imported function `imported_func()` with
 // a single parameter containing the value 42.
 // https://github.com/mdn/webassembly-examples/blob/main/js-api-examples/simple.wat
+
 const wasm = Switch.readFileSync(new URL('simple.wasm', Switch.entrypoint));
-//const wasm = require('fs').readFileSync(__dirname + '/../romfs/simple.wasm');
 
 const mod = new WebAssembly.Module(wasm);
 console.log(WebAssembly.Module.exports(mod));
@@ -11,7 +11,7 @@ console.log(WebAssembly.Module.imports(mod));
 
 const importObject = {
 	imports: {
-		imported_func(arg /* @type any */) {
+		imported_func(arg: number) {
 			console.log({ arg });
 		},
 	},
@@ -19,8 +19,8 @@ const importObject = {
 
 WebAssembly.instantiate(mod, importObject)
 	.then((instance) => {
-		console.log(Object.keys(instance.exports));
-		instance.exports.exported_func();
+		const exported_func = instance.exports.exported_func as () => void;
+		exported_func();
 	})
 	.catch((err) => {
 		console.error(err);
