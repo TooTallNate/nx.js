07c9947f6570814c20b22f6e75750ee50284892c
Nathan Rajlich <n@n8.io>
2023-09-30 00:36:31 -0300

    Update IP

--- upload.sh
+++ upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 APP="${1-hello-world}"
-SWITCH_HOST="${2-192.168.86.115:5000}"
+SWITCH_HOST="${2-192.168.1.112:5000}"
 curl --netrc-optional \
     --upload-file nxjs.nro "ftp://${SWITCH_HOST}/switch/nxjs.nro" \
     --upload-file "./apps/${APP}/romfs/main.js" "ftp://${SWITCH_HOST}/switch/nxjs.js"
