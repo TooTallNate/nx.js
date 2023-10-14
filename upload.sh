d326b8d0f3616eb9b8114c0eb850a44d16befed6
Nathan Rajlich <n@n8.io>
2023-10-14 03:59:58 -0700

    Add source map tracing in error stack traces (#48)

--- upload.sh
+++ upload.sh
@@ -1,7 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -euo pipefail
 APP="${1-hello-world}"
 SWITCH_HOST="${2-192.168.86.115:5000}"
-curl --netrc-optional \
-    --upload-file nxjs.nro "ftp://${SWITCH_HOST}/switch/nxjs.nro" \
+
+ARGS=(
+    --upload-file nxjs.nro "ftp://${SWITCH_HOST}/switch/nxjs.nro"
     --upload-file "./apps/${APP}/romfs/main.js" "ftp://${SWITCH_HOST}/switch/nxjs.js"
+)
+
+# Upload source map file, if it exists
+if [ -f "./apps/${APP}/romfs/main.js.map" ]; then
+    ARGS+=(--upload-file "./apps/${APP}/romfs/main.js.map" "ftp://${SWITCH_HOST}/switch/main.js.map")
+fi
+
+curl --netrc-optional "${ARGS[@]}"
