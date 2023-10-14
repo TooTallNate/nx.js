e9122dfb741314bfb9ab7620a598b5cad3dfca32
Nathan Rajlich <n@n8.io>
2023-10-14 03:28:30 -0700

    Remove 'conditions'

--- packages/runtime/bundle.mjs
+++ packages/runtime/bundle.mjs
@@ -4,7 +4,7 @@ await build({
 	bundle: true,
 	minify: process.env.MINIFY === '1',
 	mainFields: ['module', 'main'],
-	conditions: ['nxjs', 'import', 'browser', 'require', 'default'],
+	//conditions: ['nxjs', 'import', 'browser', 'require', 'default'],
 	target: 'es2020',
 	entryPoints: ['src/index.ts'],
 	sourcemap: true,
