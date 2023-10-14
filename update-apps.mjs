75cb8482ff3cd23cdeb883eeb1f52b0ad697d565
Nathan Rajlich <n@n8.io>
2023-10-14 02:46:25 -0700

    Ignore `.DS_Store`

--- update-apps.mjs
+++ update-apps.mjs
@@ -3,6 +3,7 @@ import fs from 'fs';
 const appsDir = new URL('apps/', import.meta.url);
 
 for (const app of fs.readdirSync(appsDir)) {
+    if (app === '.DS_Store') continue;
     const appUrl = new URL(`${app}/`, appsDir);
     // … do your thing…
 }
