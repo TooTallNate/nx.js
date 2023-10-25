5f4a7b1e5c190c707c84b77b226645279edce629
Nathan Rajlich <n@n8.io>
2023-10-25 02:11:18 -0700

    Add try/catch

--- apps/tcp-server/src/main.ts
+++ apps/tcp-server/src/main.ts
@@ -4,19 +4,25 @@ const server = Switch.listen({ port });
 
 // "accept" event occurs when a new TCP client has connected.
 server.addEventListener('accept', async ({ fd }) => {
-	console.log('Client connection established (fd=%d)', fd);
-	const buf = new ArrayBuffer(10);
-	while (1) {
-		const n = await Switch.read(fd, buf);
-		if (n === 0) {
-			console.log('Client connection closed (fd=%d)', fd);
-			break;
-		}
-		const text = new TextDecoder().decode(new Uint8Array(buf, 0, n));
-		console.log('Read %d bytes (fd=%d): %j', n, fd, text);
+	try {
+		console.log('Client connection established (fd=%d)', fd);
+		const buf = new ArrayBuffer(1024);
+		while (1) {
+			let bytes = await Switch.read(fd, buf);
+			if (bytes === 0) {
+				console.log('Client connection closed (fd=%d)', fd);
+				break;
+			}
+			const arr = new Uint8Array(buf, 0, bytes);
+			const text = new TextDecoder().decode(arr);
+			console.log('Read', { fd, text, bytes });
 
-		// Echo the input back to the client
-		await Switch.write(fd, new Uint8Array(buf, 0, n));
+			// Echo the input back to the client
+			bytes = await Switch.write(fd, arr);
+			console.log('Write', { fd, bytes });
+		}
+	} catch (err) {
+		console.error(err);
 	}
 });
 
