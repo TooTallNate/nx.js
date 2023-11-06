bc9b0260f426cc3f68f2517698500bc0286e075a
Nathan Rajlich <n@n8.io>
2023-11-05 23:34:56 -0800

    .

--- packages/runtime/src/types.ts
+++ packages/runtime/src/types.ts
@@ -1,3 +1,6 @@
+import type { SwitchClass } from './switch';
+import type { Socket } from './tcp';
+
 export type PathLike = string | URL;
 
 export interface Stats {
@@ -90,7 +93,19 @@ export interface SocketAddress {
 export type SecureTransportKind = 'off' | 'on' | 'starttls';
 
 export interface SocketOptions {
+	/**
+	 * Specifies whether or not to use TLS when creating the TCP socket.
+	 *  - `off` — Do not use TLS.
+	 *  - `on` — Use TLS.
+	 *  - `starttls` — Do not use TLS initially, but allow the socket to be upgraded to use TLS by calling {@link Socket.startTls | `startTls()`}.
+	 */
 	secureTransport?: SecureTransportKind;
+	/**
+	 * Defines whether the writable side of the TCP socket will automatically close on end-of-file (EOF).
+	 * When set to false, the writable side of the TCP socket will automatically close on EOF.
+	 * When set to true, the writable side of the TCP socket will remain open on EOF.
+	 * This option is similar to that offered by the Node.js net module and allows interoperability with code which utilizes it.
+	 */
 	allowHalfOpen?: boolean;
 }
 
