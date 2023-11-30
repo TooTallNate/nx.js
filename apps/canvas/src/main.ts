47b42a6722943075f3589340fdf00d3c1c5a2089
Nathan Rajlich <n@n8.io>
2023-11-30 01:30:17 -0800

    Use clock for "canvas" template

--- apps/canvas/src/main.ts
+++ apps/canvas/src/main.ts
@@ -1,15 +1,95 @@
 const ctx = screen.getContext('2d');
+ctx.scale(4, 4);
 
-ctx.strokeStyle = 'red';
+function clock() {
+	const now = new Date();
+	ctx.save();
+	ctx.clearRect(0, 0, 150, 150);
+	ctx.translate(75, 75);
+	ctx.scale(0.4, 0.4);
+	ctx.rotate(-Math.PI / 2);
+	ctx.strokeStyle = 'white';
+	ctx.lineWidth = 8;
+	ctx.lineCap = 'round';
 
-// Draw the ellipse
-ctx.beginPath();
-ctx.ellipse(100, 100, 50, 75, Math.PI / 4, 0, 2 * Math.PI);
-ctx.stroke();
+	// Hour marks
+	ctx.save();
+	for (let i = 0; i < 12; i++) {
+		ctx.beginPath();
+		ctx.rotate(Math.PI / 6);
+		ctx.moveTo(100, 0);
+		ctx.lineTo(120, 0);
+		ctx.stroke();
+	}
+	ctx.restore();
 
-// Draw the ellipse's line of reflection
-ctx.beginPath();
-ctx.setLineDash([5, 5]);
-ctx.moveTo(0, 200);
-ctx.lineTo(200, 0);
-ctx.stroke();
+	// Minute marks
+	ctx.save();
+	ctx.lineWidth = 5;
+	for (let i = 0; i < 60; i++) {
+		if (i % 5 !== 0) {
+			ctx.beginPath();
+			ctx.moveTo(117, 0);
+			ctx.lineTo(120, 0);
+			ctx.stroke();
+		}
+		ctx.rotate(Math.PI / 30);
+	}
+	ctx.restore();
+
+	const sec = now.getSeconds() + now.getMilliseconds() / 1000;
+	const min = now.getMinutes();
+	const hr = now.getHours() % 12;
+
+	// Write Hours
+	ctx.save();
+	ctx.rotate(
+		(Math.PI / 6) * hr + (Math.PI / 360) * min + (Math.PI / 21600) * sec
+	);
+	ctx.lineWidth = 14;
+	ctx.beginPath();
+	ctx.moveTo(-20, 0);
+	ctx.lineTo(80, 0);
+	ctx.stroke();
+	ctx.restore();
+
+	// Write Minutes
+	ctx.save();
+	ctx.rotate((Math.PI / 30) * min + (Math.PI / 1800) * sec);
+	ctx.lineWidth = 10;
+	ctx.beginPath();
+	ctx.moveTo(-28, 0);
+	ctx.lineTo(112, 0);
+	ctx.stroke();
+	ctx.restore();
+
+	// Write seconds
+	ctx.save();
+	ctx.rotate((sec * Math.PI) / 30);
+	ctx.strokeStyle = '#D40000';
+	ctx.fillStyle = '#D40000';
+	ctx.lineWidth = 6;
+	ctx.beginPath();
+	ctx.moveTo(-30, 0);
+	ctx.lineTo(83, 0);
+	ctx.stroke();
+	ctx.beginPath();
+	ctx.arc(0, 0, 10, 0, Math.PI * 2, true);
+	ctx.fill();
+	ctx.beginPath();
+	ctx.arc(95, 0, 10, 0, Math.PI * 2, true);
+	ctx.stroke();
+	ctx.restore();
+
+	ctx.beginPath();
+	ctx.lineWidth = 14;
+	ctx.strokeStyle = '#325FA2';
+	ctx.arc(0, 0, 142, 0, Math.PI * 2, true);
+	ctx.stroke();
+
+	ctx.restore();
+
+	window.requestAnimationFrame(clock);
+}
+
+window.requestAnimationFrame(clock);
