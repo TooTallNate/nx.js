e0e76e3bec576943e1e9eb3b0949e1073db23bd1
Nathan Rajlich <n@n8.io>
2023-12-15 02:32:01 -0800

    Fix hello-world

--- apps/hello-world/src/main.ts
+++ apps/hello-world/src/main.ts
@@ -2,5 +2,3 @@ console.log('Hello World!');
 console.log(' - <3 nx.js');
 console.log('');
 console.log('Press the + button to exit...');
-
-for (let i = 0; i < 10; i++) console.log(crypto.randomUUID());
