38a4c4e2b0c996cea68654c8dd4ba9dcf0e8c78b
Nathan Rajlich <n@n8.io>
2024-01-01 23:21:26 -0800

    Refactor runtime type definition generator using AST modifications (#73)

--- update-apps.mjs
+++ update-apps.mjs
@@ -3,7 +3,7 @@ import fs from 'fs';
 const appsDir = new URL('apps/', import.meta.url);
 
 for (const app of fs.readdirSync(appsDir)) {
-    if (app === '.DS_Store') continue;
-    const appUrl = new URL(`${app}/`, appsDir);
-    // … do your thing…
+	if (app === '.DS_Store') continue;
+	const appUrl = new URL(`${app}/`, appsDir);
+	// … do your thing…
 }
