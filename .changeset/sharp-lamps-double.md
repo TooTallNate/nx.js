d3339de1b5e47321a30e547fdd40baf68ded3864
Nathan Rajlich <n@n8.io>
2024-03-25 01:05:09 -0700

    Add `FsSaveDataSpaceId` enum

---
+++ .changeset/sharp-lamps-double.md
@@ -0,0 +1,5 @@
+---
+"@nx.js/constants": patch
+---
+
+Add `FsSaveDataSpaceId` enum
--- packages/constants/src/fs.ts
+++ packages/constants/src/fs.ts
@@ -1,9 +1,21 @@
+export enum FsSaveDataSpaceId {
+	System = 0, ///< System
+	User = 1, ///< User
+	SdSystem = 2, ///< SdSystem
+	Temporary = 3, ///< [3.0.0+] Temporary
+	SdUser = 4, ///< [4.0.0+] SdUser
+	ProperSystem = 100, ///< [3.0.0+] ProperSystem
+	SafeMode = 101, ///< [3.0.0+] SafeMode
+
+	All = -1, ///< Pseudo value for fsOpenSaveDataInfoReader().
+}
+
 export enum FsSaveDataType {
-  System = 0, ///< System
-  Account = 1, ///< Account
-  Bcat = 2, ///< Bcat
-  Device = 3, ///< Device
-  Temporary = 4, ///< [3.0.0+] Temporary
-  Cache = 5, ///< [3.0.0+] Cache
-  SystemBcat = 6, ///< [4.0.0+] SystemBcat
+	System = 0, ///< System
+	Account = 1, ///< Account
+	Bcat = 2, ///< Bcat
+	Device = 3, ///< Device
+	Temporary = 4, ///< [3.0.0+] Temporary
+	Cache = 5, ///< [3.0.0+] Cache
+	SystemBcat = 6, ///< [4.0.0+] SystemBcat
 }
