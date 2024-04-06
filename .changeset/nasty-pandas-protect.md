33eb3b0b7c99be7136b0d314970d61c50aab7069
Nathan Rajlich <n@n8.io>
2024-04-06 00:54:54 -0700

    Convert `console` into a `Console` class

---
+++ .changeset/nasty-pandas-protect.md
@@ -0,0 +1,5 @@
+---
+"nxjs-runtime": patch
+---
+
+Convert `console` into a `Console` class
--- packages/runtime/src/console.ts
+++ packages/runtime/src/console.ts
@@ -1,6 +1,7 @@
 import { bgRgb, bold, red, yellow } from 'kleur/colors';
 import { $ } from './$';
 import { inspect } from './inspect';
+import { createInternal } from './utils';
 
 const bgRedDim = bgRgb(60, 0, 0);
 const bgYellowDim = bgRgb(60, 60, 0);
@@ -42,6 +43,84 @@ function format(...input: unknown[]): string {
 	return s;
 }
 
+export interface ConsoleOptions {
+	print(s: string): void;
+	printErr(s: string): void;
+}
+
+const _ = createInternal<Console, ConsoleOptions>();
+
+export class Console {
+	constructor(opts: Partial<ConsoleOptions> = {}) {
+		_.set(this, {
+			print: opts.print || $.print,
+			printErr: opts.print || $.printErr,
+		});
+	}
+
+	/**
+	 * Prints string `s` to the console on the screen, without any formatting applied.
+	 * Newline is _not_ appending to the end of the string.
+	 *
+	 * @param s The text to print to the console.
+	 */
+	print(s: string) {
+		_(this).print.call(this, s);
+	}
+
+	/**
+	 * Prints string `s` to the debug log file, without any formatting applied.
+	 * Newline is _not_ appending to the end of the string.
+	 *
+	 * @param s The text to print to the log file.
+	 * @note This function **does not** invoke _text rendering mode_, so it can safely be used when rendering with the Canvas API.
+	 */
+	printErr(s: string) {
+		_(this).printErr.call(this, s);
+	}
+
+	/**
+	 * Logs the formatted `input` to the screen as white text.
+	 */
+	log(...input: unknown[]) {
+		this.print(`${format(...input)}\n`);
+	}
+
+	/**
+	 * Logs the formatted `input` to the screen as yellow text.
+	 */
+	warn(...input: unknown[]) {
+		this.print(`${bold(bgYellowDim(yellow(format(...input))))}\n`);
+	}
+
+	/**
+	 * Logs the formatted `input` to the screen as red text.
+	 */
+	error(...input: unknown[]) {
+		this.print(`${bold(bgRedDim(red(format(...input))))}\n`);
+	}
+
+	/**
+	 * Writes the formatted `input` to the debug log file.
+	 *
+	 * @note This function **does not** invoke _text rendering mode_, so it can safely be used when rendering with the Canvas API.
+	 */
+	debug(...input: unknown[]) {
+		this.printErr(`${format(...input)}\n`);
+	}
+
+	/**
+	 * Logs the formatted `input` to the screen as white text,
+	 * including a stack trace of where the function was invoked.
+	 */
+	trace(...input: unknown[]) {
+		const f = format(...input);
+		let s = new Error().stack!.split('\n').slice(1).join('\n');
+		if (!s.endsWith('\n')) s += '\n';
+		this.print(`Trace${f ? `: ${f}` : ''}\n${s}`);
+	}
+}
+
 /**
  * The global `console` object contains functions for printing text to the
  * screen, which can be useful for text-based applications, and is also
@@ -57,66 +136,4 @@ function format(...input: unknown[]): string {
  *
  * @see https://developer.mozilla.org/docs/Web/API/console
  */
-export var console = {
-	/**
-	 * Prints string `s` to the console on the screen, without any formatting applied.
-	 * Newline is _not_ appending to the end of the string.
-	 *
-	 * @param s The text to print to the console.
-	 */
-	print(s: string) {
-		$.print(s);
-	},
-
-	/**
-	 * Prints string `s` to the debug log file, without any formatting applied.
-	 * Newline is _not_ appending to the end of the string.
-	 *
-	 * @param s The text to print to the log file.
-	 * @note This function **does not** invoke _text rendering mode_, so it can safely be used when rendering with the Canvas API.
-	 */
-	printErr(s: string) {
-		$.printErr(s);
-	},
-
-	/**
-	 * Logs the formatted `input` to the screen as white text.
-	 */
-	log(...input: unknown[]) {
-		this.print(`${format(...input)}\n`);
-	},
-
-	/**
-	 * Logs the formatted `input` to the screen as yellow text.
-	 */
-	warn(...input: unknown[]) {
-		this.print(`${bold(bgYellowDim(yellow(format(...input))))}\n`);
-	},
-
-	/**
-	 * Logs the formatted `input` to the screen as red text.
-	 */
-	error(...input: unknown[]) {
-		this.print(`${bold(bgRedDim(red(format(...input))))}\n`);
-	},
-
-	/**
-	 * Writes the formatted `input` to the debug log file.
-	 *
-	 * @note This function **does not** invoke _text rendering mode_, so it can safely be used when rendering with the Canvas API.
-	 */
-	debug(...input: unknown[]) {
-		this.printErr(`${format(...input)}\n`);
-	},
-
-	/**
-	 * Logs the formatted `input` to the screen as white text,
-	 * including a stack trace of where the function was invoked.
-	 */
-	trace(...input: unknown[]) {
-		const f = format(...input);
-		let s = new Error().stack!.split('\n').slice(1).join('\n');
-		if (!s.endsWith('\n')) s += '\n';
-		this.print(`Trace${f ? `: ${f}` : ''}\n${s}`);
-	},
-};
+export var console = new Console();
