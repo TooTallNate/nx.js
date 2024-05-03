6473be5c9643eb803e4b80fef72c68231b4685f1
Nathan Rajlich <n@n8.io>
2024-05-03 01:38:33 -0700

    Add UPLOAD option to `build.sh` script

--- build.sh
+++ build.sh
@@ -17,3 +17,7 @@ make
 
 # Package app `.nro`
 pnpm nro --filter "$APP"
+
+if [ "$UPLOAD" -eq 1 ]; then
+	curl --netrc "ftp://192.168.86.115:5000/switch/${APP}.nro" --upload-file "apps/${APP}/${APP}.nro"
+fi
