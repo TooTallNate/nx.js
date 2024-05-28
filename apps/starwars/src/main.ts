bc1229a87aa1da9a1358727bce79698a83efaa3d
Nathan Rajlich <n@n8.io>
2024-05-27 18:31:35 -0700

    Use top-level await in `starwars` example

--- apps/starwars/src/main.ts
+++ apps/starwars/src/main.ts
@@ -8,7 +8,7 @@ const encoder = new TextEncoder();
 // Register "Geist Mono" font
 const fontUrl = new URL('fonts/GeistMono-Regular.otf', Switch.entrypoint);
 const fontData = Switch.readFileSync(fontUrl);
-const font = new FontFace('Geist Mono', fontData);
+const font = new FontFace('Geist Mono', fontData!);
 fonts.add(font);
 
 async function* frameIterator(
@@ -29,43 +29,39 @@ async function* frameIterator(
 	}
 }
 
-async function main() {
-	// "towel.blinkenlights.nl" no longer works on IPv4, so we'll
-	// connect to "telehack.com" and type the "starwars" command
-	const socket = Switch.connect('telehack.com:23');
-	const reader = socket.readable.getReader();
+// "towel.blinkenlights.nl" no longer works on IPv4, so we'll
+// connect to "telehack.com" and type the "starwars" command
+const socket = Switch.connect('telehack.com:23');
+const reader = socket.readable.getReader();
 
-	// wait for prompt
-	while (true) {
-		const chunk = await reader.read();
-		if (!chunk.value) throw new Error('Connection closed');
-		const value = decoder.decode(chunk.value);
-		if (value.includes('Press control-C')) {
-			break;
-		}
+// wait for prompt
+while (true) {
+	const chunk = await reader.read();
+	if (!chunk.value) throw new Error('Connection closed');
+	const value = decoder.decode(chunk.value);
+	if (value.includes('Press control-C')) {
+		break;
 	}
-
-	// write "starwars" command
-	const writer = socket.writable.getWriter();
-	await writer.write(encoder.encode('starwars\r\n'));
-
-	const ctx = screen.getContext('2d');
-	const fontSize = 30.83;
-	const yOffset = 180;
-	ctx.font = `${fontSize}px "Geist Mono"`;
-
-	// dump the movie to the console
-	for await (const frame of frameIterator(reader)) {
-		ctx.fillStyle = 'black';
-		ctx.fillRect(0, 0, screen.width, screen.height);
-		ctx.fillStyle = 'white';
-		for (let line = 0; line < frame.length; line++) {
-			const y = line * fontSize + yOffset;
-			ctx.fillText(frame[line], 0, y);
-		}
-	}
-
-	console.log('TCP socket closed');
 }
 
-main().catch(console.error);
+// write "starwars" command
+const writer = socket.writable.getWriter();
+await writer.write(encoder.encode('starwars\r\n'));
+
+const ctx = screen.getContext('2d');
+const fontSize = 30.83;
+const yOffset = 180;
+ctx.font = `${fontSize}px "Geist Mono"`;
+
+// dump the movie to the console
+for await (const frame of frameIterator(reader)) {
+	ctx.fillStyle = 'black';
+	ctx.fillRect(0, 0, screen.width, screen.height);
+	ctx.fillStyle = 'white';
+	for (let line = 0; line < frame.length; line++) {
+		const y = line * fontSize + yOffset;
+		ctx.fillText(frame[line], 0, y);
+	}
+}
+
+console.log('TCP socket closed');
