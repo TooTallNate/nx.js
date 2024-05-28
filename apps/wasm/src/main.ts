f42a71f77325dcdf620f9b64b72a2f3ff5e12f9d
Nathan Rajlich <n@n8.io>
2024-05-28 00:15:56 -0700

    Use esbuild "file" loader for `wasm` example app

--- apps/wasm/src/main.ts
+++ apps/wasm/src/main.ts
@@ -2,30 +2,23 @@
 // which invokes the imported function `imported_func()` with
 // a single parameter containing the value 42.
 // https://github.com/mdn/webassembly-examples/blob/main/js-api-examples/simple.wat
+import wasmPath from './simple.wasm';
 
-const wasm = Switch.readFileSync(
-	new URL('wasm/simple.wasm', Switch.entrypoint),
-);
+const wasm = Switch.readFileSync(new URL(wasmPath, Switch.entrypoint));
 
-const mod = new WebAssembly.Module(wasm);
+const mod = new WebAssembly.Module(wasm!);
 console.log({
 	imports: WebAssembly.Module.imports(mod),
 	exports: WebAssembly.Module.exports(mod),
 });
 
-const importObject = {
+const instance = await WebAssembly.instantiate(mod, {
 	imports: {
 		imported_func(arg: number) {
 			console.log({ arg });
 		},
 	},
-};
+});
 
-WebAssembly.instantiate(mod, importObject)
-	.then((instance) => {
-		const exported_func = instance.exports.exported_func as () => void;
-		exported_func();
-	})
-	.catch((err) => {
-		console.error(err);
-	});
+const exported_func = instance.exports.exported_func as () => void;
+exported_func();
