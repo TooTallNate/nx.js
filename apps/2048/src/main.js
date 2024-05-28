0d0cb67860dd6d606993728b1c4b50bd903d5a39
Nathan Rajlich <n@n8.io>
2024-05-28 00:53:21 -0700

    Use "file" loader for `2048` example app

--- apps/2048/src/main.js
+++ apps/2048/src/main.js
@@ -2,12 +2,14 @@ import { GameManager } from './game_manager';
 import { StorageManager } from './storage_manager';
 import { CanvasActuator } from './canvas_actuator';
 import { InputManager } from './input_manager';
+import clearSansPath from './fonts/ClearSans-Regular.ttf';
+import clearSansBoldPath from './fonts/ClearSans-Bold.ttf';
 
 const clearSansData = Switch.readFileSync(
-	new URL('fonts/ClearSans-Regular.ttf', Switch.entrypoint),
+	new URL(clearSansPath, Switch.entrypoint),
 );
 const clearSansBoldData = Switch.readFileSync(
-	new URL('fonts/ClearSans-Bold.ttf', Switch.entrypoint),
+	new URL(clearSansBoldPath, Switch.entrypoint),
 );
 fonts.add(new FontFace('Clear Sans', clearSansData));
 fonts.add(new FontFace('Clear Sans', clearSansBoldData, { weight: 'bold' }));
