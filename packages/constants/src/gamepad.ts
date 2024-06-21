e9b8187e6fa2ad948d9f60aaa52914f55968da0d
Nathan Rajlich <n@n8.io>
2024-06-20 21:00:31 -0700

    .

--- packages/constants/src/gamepad.ts
+++ packages/constants/src/gamepad.ts
@@ -1,3 +1,18 @@
+/**
+ * Mapping of the button indicies on a `Gamepad` to the
+ * corresponding name of the button on the controller.
+ *
+ * @example
+ *
+ * ```typescript
+ * import { Button } from '@nx.js/constants';
+ *
+ * const pad = navigator.getGamepads()[0];
+ * if (pad.buttons[Button.A].pressed) {
+ *   console.log('Button A is pressed');
+ * }
+ * ```
+ */
 export enum Button {
 	B,
 	A,
