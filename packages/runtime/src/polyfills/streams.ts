dffdd2db475119d52182fdba09ccceb427d39bd4
Nathan Rajlich <n@n8.io>
2024-06-21 22:46:16 -0700

    .

--- packages/runtime/src/polyfills/streams.ts
+++ packages/runtime/src/polyfills/streams.ts
@@ -130,6 +130,17 @@ export interface ReadableStreamGenericReader {
 	cancel(reason?: any): Promise<void>;
 }
 
+export interface ReadableStreamIteratorOptions {
+	/**
+	 * Asynchronously iterates over the chunks in the stream's internal queue.
+	 *
+	 * Asynchronously iterating over the stream will lock it, preventing any other consumer from acquiring a reader. The lock will be released if the async iterator's return() method is called, e.g. by breaking out of the loop.
+	 *
+	 * By default, calling the async iterator's return() method will also cancel the stream. To prevent this, use the stream's values() method, passing true for the preventCancel option.
+	 */
+	preventCancel?: boolean;
+}
+
 export interface Transformer<I = any, O = any> {
 	flush?: TransformerFlushCallback<O>;
 	readableType?: undefined;
