78b385b06aebbcab6e8211420b9808ae6fc67847
Nathan Rajlich <n@n8.io>
2024-06-26 00:34:11 -0700

    Add links + icons to root page

--- docs/app/page.tsx
+++ docs/app/page.tsx
@@ -1,5 +1,8 @@
 import Link from 'next/link';
 import Logo from './logo';
+import Discord from './discord';
+import GitHub from './github';
+import GbaTemp from './gbatemp';
 
 export default function HomePage() {
 	return (
@@ -12,12 +15,23 @@ export default function HomePage() {
 				</h2>
 			</div>
 			<p className='text-muted-foreground'>
-				You can open{' '}
+				Go to{' '}
 				<Link href='/docs' className='text-foreground font-semibold underline'>
 					/docs
 				</Link>{' '}
-				and see the documentation.
+				to read the documentation.
 			</p>
+			<div className='flex text-5xl gap-8'>
+				<a href="https://github.com/TooTallNate/nx.js" target="_blank" title="GitHub Repository">
+					<GitHub className='fill-white hover:drop-shadow-[0_0_8px_rgba(255,255,255,0.5)] transition-all' />
+				</a>
+				<a href="https://discord.gg/MMmn73nsGz" target="_blank" title="Discord Server">
+					<Discord className='fill-white hover:drop-shadow-[0_0_8px_rgba(255,255,255,0.4)] transition-all' />
+				</a>
+				<a href="https://gbatemp.net/threads/nx-js-javascript-runtime-for-nintendo-switch-homebrew-applications.639171/" target="_blank" title="GBATemp Thread">
+					<GbaTemp className='hover:drop-shadow-[0_0_8px_rgba(255,255,255,0.6)] transition-all' />
+				</a>
+			</div>
 		</main>
 	);
 }
