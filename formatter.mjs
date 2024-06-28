f25eaeae3b096f5d55390e4fd604081c8880a775
Nathan Rajlich <n@n8.io>
2024-06-28 01:27:09 -0700

    Use root-relative URLs

--- formatter.mjs
+++ formatter.mjs
@@ -1,10 +1,14 @@
 // @ts-check
+import { relative, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { MarkdownPageEvent } from 'typedoc-plugin-markdown';
 
 /**
  * @param {import('typedoc-plugin-markdown').MarkdownApplication} app
  */
 export function load(app) {
+	const root = fileURLToPath(new URL('./', import.meta.url));
+
 	// Set "title" frontmatter for each page
 	app.renderer.on(
 		MarkdownPageEvent.BEGIN,
@@ -13,6 +17,25 @@ export function load(app) {
 			page.frontmatter = {
 				title: page.model.name,
 			};
+		},
+	);
+
+	// Resolve all links to root relative
+	app.renderer.on(
+		MarkdownPageEvent.END,
+		/** @param {import('typedoc-plugin-markdown').MarkdownPageEvent} page */
+		(page) => {
+			if (!page.contents) return;
+			const rel = relative(root, page.filename);
+			const pkg = rel.split('/')[1];
+			page.contents = page.contents.replace(
+				/\[(.*?)\]\((.*?)\)/g
+				, (_, text, link) => {
+				if (!link.includes('://')) {
+					link = resolve(`/docs/api/${pkg}`, link);
+				}
+				return `[${text}](${link})`;
+			});
 		},
 	);
 
