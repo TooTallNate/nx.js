508a3ec0e85498ae04eca8550bede6cdfcbc30da
Nathan Rajlich <n@n8.io>
2024-06-30 00:26:46 -0700

    Fix docs search API endpoint

--- docs/app/source.ts
+++ docs/app/source.ts
@@ -1,11 +1,12 @@
 import { join } from 'node:path';
 import { readdirSync } from 'node:fs';
-import { fileURLToPath } from 'node:url';
 import { map } from '@/.map';
 import { createMDXSource } from 'fumadocs-mdx';
 import { loader } from 'fumadocs-core/source';
 
-const contentDir = join(fileURLToPath(import.meta.url), '../../content');
+const dirnameParts = __dirname.split('/');
+const docsDir = dirnameParts.slice(0, dirnameParts.indexOf('docs') + 1).join('/');
+const contentDir = join(docsDir, 'content');
 
 export const loaders: Map<
 	string,
