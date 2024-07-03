115f39fe025c0d3667a9acea26247b7e84193512
Nathan Rajlich <n@n8.io>
2024-07-03 03:45:58 -0700

    Add redirect test endpoints back

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -5,6 +5,30 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
 	reactStrictMode: true,
+	redirects() {
+		return [
+			{
+				statusCode: 301,
+				source: '/tests/redirect/301',
+				destination: 'https://dump.n8.io',
+			},
+			{
+				statusCode: 302,
+				source: '/tests/redirect/302',
+				destination: 'https://dump.n8.io',
+			},
+			{
+				statusCode: 307,
+				source: '/tests/redirect/307',
+				destination: 'https://dump.n8.io',
+			},
+			{
+				statusCode: 308,
+				source: '/tests/redirect/308',
+				destination: 'https://dump.n8.io',
+			},
+		];
+	},
 };
 
 export default withMDX(config);
