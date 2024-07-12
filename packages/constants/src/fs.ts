027c9ffef77d7c65cb10657e42864a82af663882
Nathan Rajlich <n@n8.io>
2024-07-12 01:51:43 -0700

    Add `BisPartitionId` enum

--- packages/constants/src/fs.ts
+++ packages/constants/src/fs.ts
@@ -17,3 +17,27 @@ export enum FsSaveDataType {
 	Cache = 5, ///< [3.0.0+] Cache
 	SystemBcat = 6, ///< [4.0.0+] SystemBcat
 }
+
+export enum BisPartitionId {
+	BootPartition1Root = 0,
+
+	BootPartition2Root = 10,
+
+	UserDataRoot = 20,
+	BootConfigAndPackage2Part1 = 21,
+	BootConfigAndPackage2Part2 = 22,
+	BootConfigAndPackage2Part3 = 23,
+	BootConfigAndPackage2Part4 = 24,
+	BootConfigAndPackage2Part5 = 25,
+	BootConfigAndPackage2Part6 = 26,
+	CalibrationBinary = 27,
+	CalibrationFile = 28,
+	SafeMode = 29,
+	User = 30,
+	System = 31,
+	SystemProperEncryption = 32,
+	SystemProperPartition = 33,
+	SignedSystemPartitionOnSafeMode = 34,
+	DeviceTreeBlob = 35,
+	System0 = 36,
+}
