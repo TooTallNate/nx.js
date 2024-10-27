ab6a0819f45a52a1268a447f72800ed3045e6f32
Nathan Rajlich <n@n8.io>
2024-10-27 00:03:07 -0700

    Add lens flare on docs screenshot

--- docs/app/screenshot.tsx
+++ docs/app/screenshot.tsx
@@ -1,21 +1,21 @@
 import Image, { ImageProps } from 'next/image';
-import SwitchImg from '@/public/switch.png';
+import SwitchImg from '@/screenshots/switch.png';
 
 export function Screenshot(props: ImageProps) {
 	return (
 		<div className='relative inline-block'>
+			<Image
+				width={1280}
+				height={720}
+				{...props}
+				className='absolute top-[8.2%] left-[17.6%] m-0 w-[64.9%]'
+			/>
 			<Image
 				src={SwitchImg}
 				alt='Screenshot'
 				width={1280}
 				height={720}
 				className='drop-shadow-md m-0'
-			/>
-			<Image
-				width={1280}
-				height={720}
-				{...props}
-				className='absolute top-[8.2%] left-[17.8%] m-0 w-[64.4%]'
 			/>
 		</div>
 	);
