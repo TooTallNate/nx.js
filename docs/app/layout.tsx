e630f615c0f613e4dbd9636a15c06ec951089de2
Nathan Rajlich <n@n8.io>
2024-11-26 00:40:54 -0800

    Formatting

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -1,6 +1,6 @@
 import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
-import localFont from 'next/font/local'
+import localFont from 'next/font/local';
 import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
 import { Analytics } from '@vercel/analytics/react';
@@ -9,12 +9,15 @@ import type { ReactNode } from 'react';
 const iconsFont = localFont({
 	src: './icons.woff2',
 	display: 'swap',
-	variable: '--font-icons'
+	variable: '--font-icons',
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
 	return (
-		<html lang='en' className={`${GeistSans.variable} ${GeistMono.variable} ${iconsFont.variable}`}>
+		<html
+			lang='en'
+			className={`${GeistSans.variable} ${GeistMono.variable} ${iconsFont.variable}`}
+		>
 			<body>
 				<RootProvider>{children}</RootProvider>
 				<Analytics />
