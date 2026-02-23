d9f31169c88085acdec7d7c9ef48d6c2e84ba5e8
Nathan Rajlich <n@n8.io>
2026-02-22 22:25:42 -0800

    Print `Switch.version` in hello-world app

--- apps/hello-world/src/main.ts
+++ apps/hello-world/src/main.ts
@@ -1,4 +1,7 @@
 console.log('Hello World!');
 console.log(' - <3 nx.js');
 console.log('');
+console.log('Current versions:');
+console.log(Switch.version);
+console.log('');
 console.log('Press the + button to exit...');
